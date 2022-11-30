ec055fb3182a483414e0d0412debf206a4e1ce0a
Ross Morgan <68851224+Ross-Morgan@users.noreply.github.com>
2022-11-30 11:34:06 +0000

    pass value array by mutable reference

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-fn binary_search(mut array: Vec<i32>, value: i32) -> u64 {
+fn binary_search(array: &mut Vec<i32>, value: i32) -> u64 {
     array.sort();
-
 
     let mut low: u64 = 0;
     let mut high: u64 = (array.len() as u64) - 1;
@@ -20,8 +19,9 @@ fn binary_search(mut array: Vec<i32>, value: i32) -> u64 {
 }
 
 fn main() {
-    let array: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    let mut array: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
     let value: i32 = 6;
-
-    println!("Value {} at index {} in array", value, binary_search(array, value));
+    let index = binary_search(&mut array);
+    
+    println!("Value {} at index {} in array", value, index);
 }
